c9ca97300f1fc0658ec985dd00af00e54149cfa0
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-25 18:58:12 +0700

    feat: Gorm

--- main.go
+++ main.go
@@ -4,22 +4,53 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+type User struct {
+	gorm.Model
+	Username string
+	Password string
+}
+
+var db *gorm.DB
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
 	io.WriteString(w, "This is my website!\n")
 }
+
 func getHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got /hello request\n")
 	io.WriteString(w, "Hello, HTTP test2!\n")
+
+	// Create
+	db.Create(&User{Username: "test", Password: "testpass"})
 }
 
 func main() {
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	newDB, err := gorm.Open(mysql.Open(DATABASE_URL), &gorm.Config{})
+
+	db = newDB
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&User{})
+
 	http.HandleFunc("/", getRoot)
 	http.HandleFunc("/hello", getHello)
 
-	err := http.ListenAndServe(":3333", nil)
+	errHttp := http.ListenAndServe(":3333", nil)
 
-	fmt.Println(err)
+	if errHttp != nil {
+		panic(errHttp)
+	}
 }
